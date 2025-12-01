bc5beda736a12755db57d4a91edee1028f69bba8
U~Iriamuzu <daviswill264@gmail.com>
2025-12-01 16:56:24 +0100

    test changes added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import SplashScreen from "./components/SplashScreen";
 import Landing from "./pages/Landing";
+// import Landing from "./components/new/Landing"
 import "./App.css";
 
 function App() {
@@ -34,6 +35,8 @@ function App() {
           <Footer />
         </>
       )}
+
+      {/* <Landing /> */}
     </div>
   );
 }
